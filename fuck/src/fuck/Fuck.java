7e5b641e02dd8f1a31d35dc2b0ede2d4dfbebc60
SkeltonMod <abgaoe@gmail.com>
2017-03-13 11:47:29 +0800

    Update Fuck.java

--- fuck/src/fuck/Fuck.java
+++ fuck/src/fuck/Fuck.java
@@ -2,52 +2,43 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package fuck;
-import javax.swing.JOptionPane; 
+package pizza;
+import javax.swing.*;
 /**
  *
  * @author CISCO 01
  */
-public class Fuck {
+public class Pizza {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   String firstNumber,thirdNumber,inputNumber, //first string entered by user 
+        // TODO code application logic here
+        String firstNumber,thirdNumber, inputNumber,//first string entered by user 
          secondNumber; // second string entered by user 
 
-float money;    // first number to add 
-float softdrink ;
-float pizza ;
-float  burger; //second number to add 
-float  change;                //sum of number1 and number2 
+int pizza,  money,   // first number to add 
+    softdrink, burger,   //second number to add 
+    change;                //sum of number1 and number2 
 
 //read in the first number from user as a string 
 inputNumber = 
-        JOptionPane.showInputDialog ( "Enter $$" ); 
-firstNumber = 
-        JOptionPane.showInputDialog ( "pizza price?" ); 
+        JOptionPane.showInputDialog ( "Enter first integer" ); 
 
-//read in the second number from user as a string 
-secondNumber = 
-        JOptionPane.showInputDialog ( "softdrink price" ); 
-thirdNumber = 
-        JOptionPane.showInputDialog ( "burger price" ); 
 
 //convert numbers from type String to type int 
+pizza = 30; 
 money = Integer.parseInt ( inputNumber); 
-pizza = Integer.parseInt ( firstNumber); 
-softdrink = Integer.parseInt ( secondNumber); 
-burger = Integer.parseInt ( thirdNumber); 
+burger = 20; 
+softdrink = 10;
 //add the numbers 
-change = money - burger - softdrink - pizza; 
+change = money - burger -softdrink - pizza; 
 
 //display the results 
 JOptionPane.showMessageDialog ( 
-null, "Your Change is " + change, "", 
+null, "The sum is " + change, "Results", 
 JOptionPane.PLAIN_MESSAGE); 
 System.exit ( 0 ); //ends the program 
     }
-    }
-
+}
